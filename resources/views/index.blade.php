97ee90c366b0e6416c9c728231a2c528776e2d23
Muhammad Zubair <muhammadzubairarain70@gmail.com>
2025-08-19 15:18:20 +0500

    Update index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -169,7 +169,7 @@
 			<div class="header-top">
 				<div class="container">
 					<div class="row justify-content-center justify-content-lg-between align-items-center gy-2">
-						<div class="col-auto d-none d-lg-block">
+						<div class="col-8 d-none d-lg-block">
 							<div class="header-links">
 								<ul>
 									<li><i class="far fa-phone"></i><a href="tel:+11156456825">+111 (564) 568 25</a></li>
@@ -178,13 +178,13 @@
 								</ul>
 							</div>
 						</div>
-						<div class="col-auto">
+						<div class="col-4">
 							<div class="header-links header-right">
 								<ul>
 									<li>
 										<div class="header-social"><span class="social-title">Follow Us:</span> <a href="https://www.facebook.com/"><i class="fab fa-facebook-f"></i></a> <a href="https://www.twitter.com/"><i class="fab fa-twitter"></i></a> <a href="https://www.linkedin.com/"><i class="fab fa-linkedin-in"></i></a> <a href="https://www.youtube.com/"><i class="fab fa-youtube"></i></a> <a href="https://www.instagram.com/"><i class="fab fa-skype"></i></a></div>
 									</li>
-									<li class="d-none d-lg-inline-block"><i class="far fa-user"></i><a href="contact.html">Login / Register</a></li>
+									{{-- <li class="d-none d-lg-inline-block"><i class="far fa-user"></i><a href="contact.html">Login / Register</a></li> --}}
 								</ul>
 							</div>
 						</div>
