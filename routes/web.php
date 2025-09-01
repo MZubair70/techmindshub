e2e922125adeb2175e5a214bc5c24ff41ada6eff
Muhammad Zubair <muhammadzubairarain70@gmail.com>
2025-09-01 19:18:19 +0500

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\ContactController;
 use Illuminate\Support\Facades\Route;
+
+// Contact Route
+Route::post('/send-contact', [ContactController::class, 'send'])->name('contact.send');
+
 
 Route::get('/', function () {
     return view('index');
-});
+})->name('home');
